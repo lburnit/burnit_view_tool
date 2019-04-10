5c10e104100c9f3ad52cad70705cb1bdcbe5aeaf
lburnit <leon.ward93@icloud.com>
2019-04-10 11:45:43 +0100

    Burnit tool initial commit

---
+++ lib/burnit_view_tool.rb
@@ -0,0 +1,6 @@
+require "burnit_view_tool/version"
+require "burnit_view_tool/renderer"
+
+module BurnitViewTool
+  # Your code goes here...
+end
---
+++ lib/burnit_view_tool/version.rb
@@ -0,0 +1,3 @@
+module BurnitViewTool
+  VERSION = "0.1.0"
+end
